512833c82acafd983eac80dd04f9565af34b56e5
Dini-n <dinatova1@gmail.com>
2023-05-02 12:05:14 +0300

    newChanges

--- server/TimeBank/Bll/converters/memberConvert.cs
+++ server/TimeBank/Bll/converters/memberConvert.cs
@@ -55,6 +55,7 @@ namespace Bll
             List<Dto.dtoClasses.member> lm = new List<Dto.dtoClasses.member>();
             microMemberList.ForEach(m => lm.Add(convertFromMicToDto(m)));
             int i = 0; 
+            //שינויים
 
             return lm;
         }
